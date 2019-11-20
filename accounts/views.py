1066b5dadc4888c73e4e5b56a3e23682db7021dc
JeesooHaa <haajeesoo@gamil.com>
2019-11-20 18:09:55 +0900

    Finish project

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm #, UserCreationForm, UserChangeForm
+# 그냥 login 이라 하면 중복 
+from django.contrib.auth import login as auth_login, logout as auth_logout, update_session_auth_hash 
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
 
-# Create your views here.
+
+# request.user.username
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # return redirect('accounts:login')
+            auth_login(request, user)
+            return redirect('movies:index')
+    else: # == 'GET'
+        form = CustomUserCreationForm()
+    context = {'form': form}
+    return render(request, 'accounts/signup.html', context)
+
+
+# session data 를 만드는 것 
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+    if request.method == 'POST':
+        # 얘만 request 가 들어간다 
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_page = request.GET.get('next')
+            # GET 요청
+            return redirect(next_page or 'movies:index')
+    else:
+        form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('movies:index')
+
+
+def index(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'accounts/index.html', context)
+
+
+def detail(request, user_pk):
+    user = get_object_or_404(User, pk=user_pk)
+    context = {'user': user}
+    return render(request, 'accounts/detail.html', context)
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST, require_GET
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
+from .models import Movie, Review, Genre
+from .forms import ReviewForm
+from  accounts.models import User
 
-# Create your views here.
+
+def index(request):
+    movies = Movie.objects.all()
+    context = { 'movies': movies }
+    return render(request, 'movies/index.html', context)
+
+
+@require_GET
+def detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    reviews = movie.reviews.all()
+    form = ReviewForm()
+    context = {
+        'movie': movie,
+        'reviews': reviews,
+        'form': form,
+    }
+    return render(request, 'movies/detail.html', context)
+
+
+@require_POST
+def create_review(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    if request.user.is_authenticated:
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.movie_id = movie_pk
+            review.user = request.user
+            review.save()
+    return redirect('movies:detail', movie_pk)
+
+
+@require_POST
+def delete_review(request, movie_pk, review_pk):
+    if request.user.is_authenticated:
+        review = get_object_or_404(Review, pk=review_pk)
+        movie = get_object_or_404(Movie, pk=movie_pk)
+        if review.user == request.user or movie.user == request.user:   
+            review.delete()
+        return redirect('movies:detail', movie_pk)
+
+
+@login_required
+def like(request, movie_pk):
+    user = request.user
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    # exists 한개의 데이터라도 존재하면 true
+    if movie.liked_users.filter(pk=user.pk).exists():
+    # if user in movie.liked_users.all():
+        user.liked_movies.remove(movie)
+        liked = False 
+    else:
+        user.liked_movies.add(movie)
+        liked = True
+    context = {
+        'liked': liked, 
+        'count': movie.liked_users.count()
+    }
+    return redirect('movies:detail', movie_pk)
+
+
+@login_required
+def follow(request, movie_pk, review_pk, user_pk):
+    # 로그인한 유저가 게시글 유저를 Follow or unfollow 한다. 
+    user = request.user
+    person = get_object_or_404(User, pk=user_pk)
+
+    if user in person.followers.all(): # 이미 팔로워다
+        person.followers.remove(user) # 언팔 
+    else:
+        person.followers.add(user) # 팔로우함
+
+    return redirect('movies:detail', movie_pk)
+
